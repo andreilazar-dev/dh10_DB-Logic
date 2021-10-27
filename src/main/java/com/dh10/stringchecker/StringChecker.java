aac46adb53c31fa9bb7998126c24502f65a1c36f
Andrei Lazar <andreilazar.dev@gmail.com>
2021-10-27 16:26:01 +0200

    fix

--- src/main/java/com/dh10/stringchecker/StringChecker.java
+++ src/main/java/com/dh10/stringchecker/StringChecker.java
@@ -1,6 +1,5 @@
 package com.dh10.stringchecker;
 
-import com.dh10.stringchecker.controller.string.StringCheckerDef;
 import com.dh10.stringchecker.utils.DBaccess;
 
 public abstract class StringChecker {
